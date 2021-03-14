70f86a1c7ce232ae3faa2fa951a4cc86c657ad99
SitanHuang <978494543.qq54@gmail.com>
2021-03-13 19:10:31 -0800

    load & save books

--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
 var fs = require('fs');
 
+if (fs.existsSync('test/dump.2020.ledg')) fs.unlinkSync('test/dump.2020.ledg');
+if (fs.existsSync('test/dump.2021.ledg')) fs.unlinkSync('test/dump.2021.ledg');
+
 // bin/ledger is included here:
 const TEST = true; // prevent CLI from running
 eval(fs.readFileSync('bin/ledger').toString());
+fs_book_name = 'test/book'
 
 
 let entry = entry_create(
@@ -31,12 +35,23 @@ describe('Entry', function() {
     it('Should have 8 char uuid', function() {
       assert.equal(entry.uuid.length, 8);
     });
+    it('Should auto balance last transfer', async function() {
+      data_init_data();
+      let e2 = entry_create("test2", [
+        ['', "Expense.Food.Coffee", 25],
+        ['', "Expense.Food", 50],
+        ['', "Assets.Checking", -20.52],
+        ['', "Liability.CreditCard", 0]
+      ]);
+      assert.equal(e2.transfers[3][2], -54.48);
+    });
   });
 });
 
 describe('Data', function () {
   describe('#data_open_books', function() {
     it('Should have 2017-2022 opened', async function() {
+      data_init_data();
       await data_open_books(data_books_required(Date.parse('2017-3-5'), Date.parse('2021-3-5')));
       assert.equal(data.booksOpened[2017], DATA_BOOK_OPENED);
       assert.equal(data.booksOpened[2018], DATA_BOOK_OPENED);
@@ -60,6 +75,29 @@ describe('Data', function () {
       assert.equal(data.books[y][0], entry);
     });
   });
+  describe('#data_remove_entry', function() {
+    it('Should remove entry', async function() {
+      data_init_data();
+      let entries = [entry, entry_create("Test2", [['', 'Exp', 0]])];
+      entries.push(JSON.parse(JSON.stringify(entry)));
+      entries[2].time = Date.parse('2017-01-11') / 1000 + 1;
+      await data_push_entries(entries);
+      assert.equal(data.books[y].length, 2);
+      assert.equal(data.books[2017].length, 1);
+      await data_remove_entry(entries[2]);
+      assert.equal(data.books[2017].length, 0);
+      await data_remove_entry(entries[1]);
+      assert.equal(data.books[y].length, 1);
+      assert.equal(data.books[y][0].description, "Test");
+    });
+    it('Should open book and set dirty', async function() {
+      data_init_data();
+      assert.ok(!data.books[y] && !data.booksOpened[y]);
+      await data_remove_entry(entry);
+      assert.ok(data.books[y]);
+      assert.equal(data.booksOpened[y], DATA_BOOK_DIRTY);
+    });
+  });
   describe('#data_push_entries', function () {
     it('Should open books in between and push entries', async function() {
       data_init_data();
@@ -73,6 +111,70 @@ describe('Data', function () {
       assert.equal(data.booksOpened[2017], DATA_BOOK_DIRTY);
       assert.equal(data.books[y][0], entries[0]);
       assert.equal(data.books[2017][0], entries[1]);
+    });
+    it('Should intialize accounts', async function() {
+      data_init_data();
+      await data_push_entry(entry);
+      assert.ok(data.accounts["Expense.Taxes.Federal"]);
+      assert.ok(data.accounts["Assets.Checking"]);
+      assert.ok(data.accounts["Liability.CC"]);
+    });
+  });
+});
+
+describe('FS', function() {
+  describe('#isNumberAtIndex', function() {
+    it('Should identify numbers', function() {
+      let str = '0123456789';
+      for (let i = 0;i < str.length;i++)
+        assert.ok(isNumberAtIndex(str, i));
+      str = "a;l skdj;fak";
+      for (let i = 0;i < str.length;i++)
+        assert.ok(!isNumberAtIndex(str, i));
+    });
+  });
+  describe('#fs_read_book, #fs_write_books', function() {
+    data_init_data();
+    it('Should parse entries and write all books', async function() {
+      // await fs_read_book(2020);
+      await data_open_books([2020]);
+      assert.equal(data.books[2020][0].time * 1000, Date.parse('2020-03-14T00:00:00+00:00'));
+      assert.equal(data.books[2020][0].description, "Test");
+      assert.equal(data.books[2020][0].uuid, "UocjnJc1");
+      assert.equal(data.books[2020][0].float, 3.14159);
+      assert.equal(data.books[2020][0].string, "123");
+      assert.equal(data.books[2020][0].array[0], 1);
+      assert.equal(data.books[2020][0].array[2], 3);
+      assert.equal(data.books[2020][0].transfers[3][0], '');
+      assert.equal(data.books[2020][0].transfers[3][1], 'auto');
+      assert.equal(data.books[2020][0].transfers[3][2], -50.51);
+
+      assert.equal(data.books[2020][1].transfers[2][2], -1500);
+      assert.equal(data.books[2020][1].transfers[2][1], data_acc_imb);
+      assert.equal(data.books[2020][1].transfers[2][0], '');
+
+      assert.equal(data.books[2020][2].transfers[3][2], -50.51);
+      assert.equal(data.books[2020][2].transfers[3][1], 'auto');
+      assert.equal(data.books[2020][2].transfers[3][0], 'some description; here asd 123');
+
+      assert.equal(data.books[2020][3].time * 1000, Date.parse('2020-12-04T00:00:00+00:00'));
+      assert.equal(data.books[2020][3].description, "public static void main(){}###");
+      assert.equal(data.books[2020][3].uuid, "UocjnJc2");
+      assert.equal(data.books[2020][3].float, 3.14159);
+      assert.equal(data.books[2020][3].string, "123");
+      assert.equal(data.books[2020][3].object.a, "b");
+      assert.equal(data.books[2020][3].transfers[3][1], 'Imbalance1');
+      assert.equal(data.books[2020][3].transfers[3][2], -50.5);
+
+      data.booksOpened[2020] = DATA_BOOK_DIRTY;
+
+      data_push_entry(entry);
+
+      // export
+      fs_book_name = 'test/dump';
+      await fs_write_books();
+      assert.ok(fs.existsSync('test/dump.2020.ledg'));
+      assert.ok(fs.existsSync('test/dump.2021.ledg'));
     });
   });
 });
