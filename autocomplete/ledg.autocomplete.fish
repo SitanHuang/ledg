7e89a4a7532973c7fe0f106d5f5438f2c74134a0
SitanHuang <978494543.qq54@gmail.com>
2021-04-28 09:58:38 -0700

    basic subcommands completion

--- autocomplete/ledg.autocomplete.fish
+++ autocomplete/ledg.autocomplete.fish
@@ -2,7 +2,22 @@ set -l ledg_commands edit accounts burndown close register history balancesheet\
        balancesheetequity cashflow incomestatement info tags add modify\
        delete budget print git eval stats count export
 
+# disable file completions
 complete -c ledg -f
 
+# root subcommands
 complete -c ledg -n "not __fish_seen_subcommand_from $ledg_commands" \
     -a "$ledg_commands"
+
+# ======= level 2 subcommands =======
+complete -c ledg -n "__fish_seen_subcommand_from accounts" \
+    -a "add rename"
+
+complete -c ledg -n "__fish_seen_subcommand_from info" \
+    -a "flat"
+
+complete -c ledg -n "__fish_seen_subcommand_from export" \
+    -a "gnucash-transactions gnucash-accounts"
+
+complete -c ledg -n "__fish_seen_subcommand_from git" \
+    -a "checkout add commit push pull merge cherry-pick"
