3738f01c1da106a0d192fbc4d723815d98adaef5
SitanHuang <978494543.qq54@gmail.com>
2021-04-28 10:31:12 -0700

    basic options auto complete

--- autocomplete/ledg.autocomplete.fish
+++ autocomplete/ledg.autocomplete.fish
@@ -2,22 +2,44 @@ set -l ledg_commands edit accounts burndown close register history balancesheet\
        balancesheetequity cashflow incomestatement info tags add modify\
        delete budget print git eval stats count export
 
+set -l ledg_options "--file= -F --light-theme --lt --csv --html --format= --transpose"\
+       "--budget= --currency= --valuation-date= -valuation-eop --income= --expense="\
+       "--equity= --asset= -i --confirm --alias- --real --q1 --q2 --q3 --abs"\
+       "--count --cumulative --cml --account= --sort --invert --hide-zero --hz"\
+       "--skip --cumulative-columns= --epoch --avg --isofull --iso --daily --weekly"\
+       "--biweekly --monthly --quarterly --yearly --sum --skip-book-close --sbc"\
+       "-% --percent --max-depth= --depth= --dep= --sum-parent --sp --tree --field="\
+       "--date= -y --default-pending --add-tag= --remove-tag= --set-mod= --prices"\
+       "--remove-mod= --do-not-adjust --simple --ledger --show-default-currency"\
+       "--prices-only --pad-spaces="
+
+
 # disable file completions
 complete -c ledg -f
 
+# options
+complete -c ledg -n "__fish_seen_subcommand_from $ledg_commands" \
+    -a "$ledg_options"
+
 # root subcommands
 complete -c ledg -n "not __fish_seen_subcommand_from $ledg_commands" \
-    -a "$ledg_commands"
+    -a "$ledg_commands $ledg_options"
 
 # ======= level 2 subcommands =======
 complete -c ledg -n "__fish_seen_subcommand_from accounts" \
-    -a "add rename"
+    -a "add rename $ledg_options"
 
 complete -c ledg -n "__fish_seen_subcommand_from info" \
-    -a "flat"
+    -a "flat $ledg_options"
+
+complete -c ledg -n "__fish_seen_subcommand_from edit" \
+    -a "new $ledg_options"
+
+complete -c ledg -n "__fish_seen_subcommand_from budget" \
+    -a "edit list $ledg_options"
 
 complete -c ledg -n "__fish_seen_subcommand_from export" \
-    -a "gnucash-transactions gnucash-accounts"
+    -a "gnucash-transactions gnucash-accounts $ledg_options"
 
 complete -c ledg -n "__fish_seen_subcommand_from git" \
-    -a "checkout add commit push pull merge cherry-pick"
+    -a "checkout add commit push pull merge cherry-pick $ledg_options"
