7a1a503f5e6c3c773fa69e26a1e9fc28cb22a03d
Sitan Huang <978494543.qq54@gmail.com>
2025-04-07 12:43:44 -0500

    LedgObjectBuilder

--- lib/core/data/ledgObject.ts
+++ lib/core/data/ledgObject.ts
@@ -1,15 +1,65 @@
 
-import { SourceDescriptor } from "./sourceDescriptor.ts";
+import { NullSourceDescriptor, SourceDescriptor } from "./sourceDescriptor.ts";
 import { timestamp } from '../types.ts';
+import { nanoid } from "../legacy/nanoid.ts";
 
 export type Metadata = Record<string, unknown>;
 
 export type UUID = string;
 
 export interface LedgObject {
-  id: UUID;
-  date: timestamp,
-  date2: timestamp,
-  source: SourceDescriptor;
-  metadata: Metadata;
+  readonly id: UUID;
+  readonly date: timestamp,
+  readonly date2: timestamp,
+  readonly source: SourceDescriptor;
+  readonly metadata: Metadata;
+}
+
+export abstract class LedgObjectBuilder<T extends LedgObject> {
+  protected id?: UUID;
+  protected source: SourceDescriptor = NullSourceDescriptor.INSTANCE;
+  protected metadata: Metadata = {};
+  protected date?: timestamp;
+  protected date2?: timestamp;
+
+  withId(id: UUID): this {
+    this.id = id;
+    return this;
+  }
+
+  genId(): this {
+    this.id = nanoid(8);
+    return this;
+  }
+
+  withSource(source: SourceDescriptor): this {
+    this.source = source;
+    return this;
+  }
+
+  withMetadata(metadata: Metadata): this {
+    this.metadata = metadata;
+    return this;
+  }
+
+  withDate(date: timestamp): this {
+    this.date = date;
+    return this;
+  }
+
+  withDate2(date2: timestamp): this {
+    this.date2 = date2;
+    return this;
+  }
+
+  isBuildable(): boolean {
+    return (
+      this.id != null &&
+      this.date != null &&
+      this.date2 != null &&
+      this.source != null &&
+      this.metadata != null);
+  }
+
+  abstract build(): T;
 }
